94666b03461402851d50c5457554ba5b0b4815a7
MatteoArMed <mate.araneda@duocuc.cl>
2023-06-29 20:25:11 -0400

    Ultimo Pull

--- codigo/Prueba01/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Genero(models.Model):
-    id_genero = models.AutoField(db_column='idGenero',prymary_ket=True)
-    genero = models.CharField(max_length=20,blank=False,null=False)
